e0e104a55cdf2e5d76df9aa50ac793036db5d673
Thom Leiter <thom@40degreesmedia.com>
2017-03-05 21:49:14 -0500

    Added plus button segue

---
+++ DoIt/task.swift
@@ -0,0 +1,14 @@
+//
+//  task.swift
+//  DoIt
+//
+//  Created by Leiter Family on 3/5/17.
+//  Copyright © 2017 40° Degrees Media, Ltd. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
